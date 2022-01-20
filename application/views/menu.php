489bffd093298f3cf56c99345023561d3df22720
SNoguera02 <sanf472@gmail.com>
2022-01-20 02:03:05 -0500

    Acerca de en el menu

--- application/views/menu.php
+++ application/views/menu.php
@@ -21,5 +21,8 @@
 
 <button onclick="usuarios()" class="botonmenu2" type="Submit"> <b class="letter">Usuarios</b> <img class="registro_login" src="<?php echo base_url('assets/img/usuario.png');?>"></button> 
 
+
+<button  class="botonmenu2" href="" id=""> <b class="letter">Acerca de</b> <img style="height: 112px;" src="<?php echo base_url('assets/img/acerca-de.png');?>"></button> 
+
 <button  class="botonmenu2" href="" id="cerrarSession"> <b class="letter">Cerrar Sesión</b> <img style="height: 112px;" src="<?php echo base_url('assets/img/salir.png');?>"></button> 
 </div>
