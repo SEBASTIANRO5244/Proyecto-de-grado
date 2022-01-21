4fd65d0e1ed84dbf5e0fe7d8cc17a29931bae594
SNoguera02 <sanf472@gmail.com>
2022-01-21 16:27:44 -0500

    Reporte de acerca de

---
+++ application/views/treportes/TRAcerca_de.php
@@ -0,0 +1,20 @@
+<head>
+  <img style="width: 25%; height: 15%; margin-left:-20px;" src="assets/img/logoStudent.png">
+  <div style="text-align:center; margin-left:50px; padding:80px; margin-top:-160px">
+  <h2><b>ACERCA DE</b></h2>
+  <h3><b>"Si se puede IMAGINAR, se puede PROGRAMAR"</b></h3>
+</div>
+
+<div style=" text-align: justify; margin-top: -45px; line-height: 220%;">
+
+<p>STUDENTMAT, es un software desarrollado por SEBASTIAN ANDRES RODRIGUEZ DOMINGUEZ y SEBASTIAN ANDRES NOGUERA FONTALVO, dos grandes y apasionados jóvenes estudiantes del POLITECNICO DE LA COSTA ATLANTICA por el desarrollo de aplicaciones informáticas que ayuden a mejorar procesos de grandes y medianas empresas.</p>
+
+<p> El desarrollo de este software surge a idea de estos estudiantes quienes evidenciaron una gran problemática en el sector educativo, específicamente en escuelas privadas pequeñas, quienes no cumplen con procesos que ayuden a mejorar su tiempo de respuesta a la comunidad educativa.</p>
+
+<p>SEBASTIAN ANDRES RODRIGUEZ DOMINGUEZ y SEBASTIAN ANDRES NOGUERA FONTALVO, buscan innovar, crear, aprender y desarrollar estrategias tecnológicas que impacten en cada uno de los sectores en el mundo, buscando ser pilotos en la mejora continua de procesos empresariales a través de la implementación de tecnologías de la vanguardia y creaciones netamente nuevas en el diario a vivir.
+</p>
+
+</div>
+
+</head>
+
